2c25730cfc04983d6aec11cd97361022e30d5733
fadriano <felipeadrianosg@gmail.com>
2016-11-18 15:13:51 -0300

    first commit

---
+++ getpdfspened.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get all pids of okular
+pidofprogram=(`ps -C okular | grep -Po '^ [\d]+'`)
+
+foldername=`date +'%d-%m-%y'`
+
+mkdir $foldername
+cd $foldername
+
+# for each pid of okular get what pdf is opened
+for pid in "${pidofprogram[@]}"
+do
+#	echo $pid
+	pdffiles=`lsof -p $pid | grep pdf`
+
+	for pdf in "${pdffiles[@]}" 
+	do
+		#echo $pdf
+		pdffile=`echo $pdf | grep -Po  '(?<=\/).*(?=pdf)'`
+		echo "/"$pdffile"pdf" >> pdfsopened
+
+	done
+done
